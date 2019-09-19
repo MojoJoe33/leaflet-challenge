62955b39bb15306a260f9bb08480de325d6f8e1c
MojoJoe33 <50093354+MojoJoe33@users.noreply.github.com>
2019-09-19 16:14:27 -0500

    Add files via upload

---
+++ Leaflet-step-1/static/js/config.js
@@ -0,0 +1,2 @@
+// API key
+const API_KEY = "pk.eyJ1IjoibW9qb2pvZSIsImEiOiJjazA5cGQyNzEwM2M1M2pxdm51ZjJ0ZGM0In0.gNeR0dpxJ__RIS0-LXoV0A";
---
+++ Leaflet-step-1/static/js/logic.js
@@ -0,0 +1,107 @@
+let queryUrl = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson";
+
+function markerSize(mag) {
+    return mag * 30000;
+  }
+
+function markerColor(mag) {
+    if (mag <= 1) {
+        return "#ADFF2F";
+    } else if (mag <= 2) {
+        return "#9ACD32";
+    } else if (mag <= 3) {
+        return "#FFFF00";
+    } else if (mag <= 4) {
+        return "#ffd700";
+    } else if (mag <= 5) {
+        return "#FFA500";
+    } else {
+        return "#FF0000";
+    };
+  }
+
+d3.json(queryUrl, function(data) {
+  createFeatures(data.features);
+});
+
+function createFeatures(earthquakeData) {
+
+    function onEachFeature(feature, layer) {
+      layer.bindPopup("<h3>" + feature.properties.place +
+        "</h3><hr><p>" + new Date(feature.properties.time) + "</p>");
+    }
+
+    let earthquakes = L.geoJSON(earthquakeData, {
+      onEachFeature:function(feature, layer) {
+
+    layer.bindPopup("<h3>" + feature.properties.place +
+      "</h3><hr><p>" + new Date(feature.properties.time) + "</p>" + "<p> Magnitude:" +  feature.properties.mag + "</p>")
+    },     pointToLayer: function (feature, latlng) {
+      return new L.circle(latlng,
+        {radius: markerSize(feature.properties.mag),
+        fillColor: markerColor(feature.properties.mag),
+        fillOpacity: 1,
+        stroke: false,
+    })
+  }
+  });
+
+  createMap(earthquakes);
+}
+
+function createMap(earthquakes) {
+
+  let streetmap = L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
+    attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
+    maxZoom: 18,
+    id: "mapbox.streets",
+    accessToken: API_KEY
+  });
+
+  let darkmap = L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
+    attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
+    maxZoom: 18,
+    id: "mapbox.dark",
+    accessToken: API_KEY
+  });
+
+  let baseMaps = {
+    "Street Map": streetmap,
+    "Dark Map": darkmap
+  };
+
+  let overlayMaps = {
+    Earthquakes: earthquakes
+  };
+
+  let myMap = L.map("map", {
+    center: [
+      37.09, -95.71
+    ],
+    zoom: 5,
+    layers: [streetmap, earthquakes]
+  });
+
+  L.control.layers(baseMaps, overlayMaps, {
+    collapsed: false
+  }).addTo(myMap);
+
+let legend = L.control({position: 'bottomright'});
+
+legend.onAdd = function() {
+
+    let div = L.DomUtil.create('div', 'info legend'),
+        magnitudes = [0, 1, 2, 3, 4, 5];
+
+    for (let i = 0; i < magnitudes.length; i++) {
+        div.innerHTML +=
+            '<i style="background:' + markerColor(magnitudes[i] + 1) + '"></i> ' + 
+    + magnitudes[i] + (magnitudes[i + 1] ? ' - ' + magnitudes[i + 1] + '<br>' : ' + ');
+    }
+
+    return div;
+};
+
+legend.addTo(myMap);
+
+}
